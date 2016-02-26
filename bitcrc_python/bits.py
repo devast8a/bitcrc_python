00e9a530d634b9ab3875b7d3dca499145bac9a50
devast8a <devast8a@whitefirex.com>
2016-02-26 10:36:57 +1030

    Add function to find last set bit to bits

--- bitcrc_python/bits.py
+++ bitcrc_python/bits.py
@@ -1,5 +1,5 @@
 def set(bit):
-    """Set the specifeid bit eg. set(8) == 0x80"""
+    """Set the specifeid bit (1-indexed) eg. set(8) == 0x80"""
     return 1 << (bit - 1)
 
 def setN(n):
@@ -7,9 +7,18 @@ def setN(n):
     return set(n + 1) - 1
 
 def reverse(value, length):
+    """Reverse an integer value with length bits eg. reverse(0b10,2) == 0b01"""
     output = 0
     for bit in range(length):
         if value & set(bit + 1) != 0:
             output |= set(length - bit)
     return output
 
+def find_last_set(value):
+    """ Returns the position of the last set bit (1-indexed)
+        eg. find_last_set(0x8012) == 16 """
+    output = 0
+    while value > 0:
+        value >>= 1
+        output += 1
+    return output
